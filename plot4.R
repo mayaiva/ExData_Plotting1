97162301d033882a7137ebd489158ac141ec3e5e
mayaiva <mayaivanova@hotmail.com>
2017-03-11 21:13:03 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -5,7 +5,7 @@ data <- read.table(data_file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=
 ##Select 1st and 2nd of February 2007
 data_subset <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
 
-## Convert  the date and time information 
+## Convert the date and time variables
 datetime <- strptime(paste(data_subset$Date, the_data_subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 
 Global_active_power <- as.numeric(data_subset$Global_active_power)
