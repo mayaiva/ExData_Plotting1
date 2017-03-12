79470ecc5e16d5f15b22044416f831b8f4cc075b
mayaiva <mayaivanova@hotmail.com>
2017-03-11 21:14:19 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -5,7 +5,7 @@ data <- read.table(data_file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=
 ##Select 1st and 2nd of February 2007
 data_subset <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
 
-## Convert the date and time variables
+##Convert the date and time variables
 datetime <- strptime(paste(data_subset$Date, the_data_subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 
 Global_active_power <- as.numeric(data_subset$Global_active_power)
@@ -19,15 +19,15 @@ png("plot4.png", width=480, height=480)
  
 par(mfrow = c(2, 2))
 
-## Create 1st plot
+##Create 1st plot
 plot(datetime, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)", cex=0.2)
 #end upper left plot
  
-## Create 2nd plot
+##Create 2nd plot
 voltage <- as.numeric(data_subset$Voltage)
 plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
 
-## Create 3rd plot
+##Create 3rd plot
 plot(datetime, the_subMetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, the_subMetering2, type="l", col="red")
 lines(datetime, the_subMetering3, type="l", col="blue")
